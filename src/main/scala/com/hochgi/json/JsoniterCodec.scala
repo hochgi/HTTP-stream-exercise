dbe7571403bd3f518315ab010a2264a0f10feb09
hochgi <giladhoch@yahoo.com>
2019-01-01 15:54:10 +0200

    trying FQCNs to workaround jsoniter known issue

--- src/main/scala/com/hochgi/json/JsoniterCodec.scala
+++ src/main/scala/com/hochgi/json/JsoniterCodec.scala
@@ -2,22 +2,23 @@ package com.hochgi.json
 
 import com.hochgi.json.EventJsonParser.{Event, Stats}
 
-// imports are nested because of:
+// import is nested & FQCNs because of:
 // https://github.com/plokhotnyuk/jsoniter-scala#known-issues
 object JsoniterCodec {
 
   import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
 
   implicit val eventCodec: JsonValueCodec[Event] = {
-    import com.github.plokhotnyuk.jsoniter_scala.macros.{JsonCodecMaker, CodecMakerConfig}
-    JsonCodecMaker.make[Event](CodecMakerConfig.apply(fieldNameMapper = {
+    com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker.make[Event](
+      com.github.plokhotnyuk.jsoniter_scala.macros.CodecMakerConfig(fieldNameMapper = {
       case "eventType" => "event_type"
       case anythingElse => anythingElse
     }))
   }
 
   implicit val statsCodec: JsonValueCodec[Stats] = {
-    import com.github.plokhotnyuk.jsoniter_scala.macros.{JsonCodecMaker, CodecMakerConfig}
-    JsonCodecMaker.make[Stats](CodecMakerConfig())
+    com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker.make[Stats](
+      com.github.plokhotnyuk.jsoniter_scala.macros.CodecMakerConfig()
+    )
   }
 }
